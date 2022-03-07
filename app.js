38401bdb71238ee4b81a497d7538f360d4f01282
jiji <jiji@localhost.localdomain>
2022-03-08 00:18:30 +0700

    Update3

--- app.js
+++ app.js
@@ -3,38 +3,32 @@ let popCount = 0;
 
 var popSound = new Audio('sound/mixkit-message-pop-alert-2354 (mp3cut.net).mp3');
 
-function imgChange() {
-    var Image_Id = document.getElementById('chi');
-    if (Image_Id.src.match("images/chi1.png")) {
-        Image_Id.src = "images/chi2.png";
-    }
-    else {
-        Image_Id.src = "images/chi1.png";
-    }
-}        
+// function imgChange() {
+//     var Image_Id = document.getElementById('chi');
+//     if (Image_Id.src.match("images/chi1.png")) {
+//         Image_Id.src = "images/chi2.png";
+//     }
+//     else {
+//         Image_Id.src = "images/chi1.png";
+//     }
+// }        
 
 function press() {
     document.getElementById("chi").src="images/chi2.png";
-    setTimeout(function (){
+    // setTimeout(function (){
         popCount++;
         document.getElementById("counter").innerHTML = popCount;
         popSound.play();
-    }, 100); 
+    // }, 500); 
         
 }
   
 function release() {
-    document.getElementById("chi").src="images/chi1.png";
+    setTimeout(function (){
+        document.getElementById("chi").src="images/chi1.png";
+    }, 100); 
+    // document.getElementById("chi").src="images/chi1.png";
 }
 
-  function pop() {
-    // if (popChange === 1) {
-    //     popChange = 2;
-    // } else {
-    //     popChange = 1;
-    // }
-    // alert("pop");
-    document.getElementById("chi").src="images/chi2.png";
-}
 // document.getElementById("chi").addEventListener("click", pop);
 
