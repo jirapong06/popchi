c4a05baf43813534198b0557e870312ca1b941a9
jiji <jiji@localhost.localdomain>
2022-03-07 23:23:31 +0700

    Update2

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ let popCount = 0;
 
 var popSound = new Audio('sound/mixkit-message-pop-alert-2354 (mp3cut.net).mp3');
 
+function imgChange() {
+    var Image_Id = document.getElementById('chi');
+    if (Image_Id.src.match("images/chi1.png")) {
+        Image_Id.src = "images/chi2.png";
+    }
+    else {
+        Image_Id.src = "images/chi1.png";
+    }
+}        
+
 function press() {
     document.getElementById("chi").src="images/chi2.png";
-    popSound.play();
-    popCount++;
-    document.getElementById("counter").innerHTML = popCount;
+    setTimeout(function (){
+        popCount++;
+        document.getElementById("counter").innerHTML = popCount;
+        popSound.play();
+    }, 100); 
+        
 }
   
 function release() {
